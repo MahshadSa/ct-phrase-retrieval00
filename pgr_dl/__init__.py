4899e55bdb4bf1ecec44903a36ebc7cfc419a1e5
Mahshad <mahshad_sarikhani@yahoo.com>
2025-10-17 22:08:47 +0330

    Delete pgr_dl directory

--- pgr_dl/__init__.py
+++
@@ -1,5 +0,0 @@
-"""DeepLesion-specific adapters, IO, windowing, phrases, and evaluation."""
-
-from . import adapters, eval_dl, io_deeplesion, phrases, windowing  # noqa: F401
-
-__all__ = ["adapters", "eval_dl", "io_deeplesion", "phrases", "windowing"]
